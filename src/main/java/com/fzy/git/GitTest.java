f928b41baa15a3007186ac7fe4914a86481a0958
Fzy <1119759672@qq.com>
2021-12-23 16:39:15 +0800

    third commit

--- src/main/java/com/fzy/git/GitTest.java
+++ src/main/java/com/fzy/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git - test");
         System.out.println("git - second");
+        System.out.println("git - hello3");
     }
 
 }
