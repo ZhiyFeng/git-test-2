761406354d6e56dd0a93efb9b6b539b3cef2ecb0
Fzy <1119759672@qq.com>
2021-12-23 16:35:49 +0800

    first commit

---
+++ src/main/java/com/fzy/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.fzy.git;
+
+/**
+ * @Author Lenovo
+ * @Date 2021/12/23 16:32
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("git - test");
+    }
+
+}
