b21e2e6ef1ae5cfe0a3f6c7d536a0afa83ed2e33
Fzy <1119759672@qq.com>
2021-12-23 16:48:15 +0800

    hot-fix commit - first

--- src/main/java/com/fzy/git/GitTest.java
+++ src/main/java/com/fzy/git/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("git - test");
         System.out.println("git - second");
         System.out.println("git - hello3");
+        System.out.println("git - hot - fix");
     }
 
 }
